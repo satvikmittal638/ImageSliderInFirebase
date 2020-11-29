506c5bb4ccbed4bfc5b40f201ffbe49c7016e9da
Satvik Mittal <satvikmittal638@gmail.com>
2020-11-29 16:19:10 +0530

    Added OnClick listener

--- app/src/main/java/com/example/imagesliderinfirebase/MainActivity.java
+++ app/src/main/java/com/example/imagesliderinfirebase/MainActivity.java
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.interfaces.ItemClickListener;
 import com.denzcoskun.imageslider.models.SlideModel;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -29,18 +31,27 @@ ImageSlider slider;
 
         FirebaseDatabase.getInstance().getReference().child("ImageSlider")
                 .addListenerForSingleValueEvent(new ValueEventListener() {
+                    //everything relating to the imageSlider is to be done here
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
                         for(DataSnapshot d:snapshot.getChildren())
                             slideModelList.add(new SlideModel(d.child("url").getValue().toString(),d.child("title").getValue().toString(), ScaleTypes.FIT));
                     slider.setImageList(slideModelList);
-                    }
 
+                        slider.setItemClickListener(new ItemClickListener() {
+                            @Override
+                            public void onItemSelected(int i) {
+                                //gets the object at 'i' index from the slideModelList
+                                Toast.makeText(getApplicationContext(),"title: "+slideModelList.get(i).getTitle(),Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
                     @Override
                     public void onCancelled(@NonNull DatabaseError error) {
 
                     }
                 });
+
 
     }
 }
